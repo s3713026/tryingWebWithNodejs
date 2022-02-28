d7658c30959484562733cd1d4b605123cd3749f3
unknown <phucuong200297@gmail.com>
2022-02-28 09:41:33 +0700

    fix server

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ io.on('connection',(socket)=>{
     console.log('a user connected')
 })
 
-var server = app.listen(3000, ()=> {
+var server = app.listen(process.env.PORT || 3000, ()=> {
     console.log('Server is listening on port',server.address().port)
 })
 

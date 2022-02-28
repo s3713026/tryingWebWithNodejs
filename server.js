e4e34caa08a52f3ed6368ba1bf56e6798ffcc78c
Phu cuong Truong <phucuong200297@gmail.com>
2022-02-28 22:52:38 +0700

    update0

--- server.js
+++ server.js
@@ -4,26 +4,33 @@ var app = express()
 var http = require('http').Server(app)
 // var io = require('socket.io')(3000)
 
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-var messages =[
-    {name: 'Cuong',message:'Hi'},
-    {name: 'Cuong2', message:'Hello'}
-]
-
-app.get('/messages',(req,res)=>{
-    res.send(messages)
-})
-
-app.post('/messages',(req,res)=>{
-    messages.push(req.body)
-    // io.emit('message',req.body)
-    res.sendStatus(200)
-})
 app.get('/',(req,res)=>{
-    console.log(window.location.href)
+    var url_page = req.query;
+    var string = JSON.stringify(url_page);
+    var objectValue = JSON.parse(string);
+    var get_authorization_code = objectValue['code'];
+    console.log("Authorization Code: " + get_authorization_code);
+    res.send(get_authorization_code);
 })
+
+// app.use(express.static(__dirname))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({extended:false}))
+// var messages =[
+//     {name: 'Cuong',message:'Hi'},
+//     {name: 'Cuong2', message:'Hello'}
+// ]
+
+// app.get('/messages',(req,res)=>{
+//     res.send(messages)
+// })
+
+// app.post('/messages',(req,res)=>{
+//     messages.push(req.body)
+//     // io.emit('message',req.body)
+//     res.sendStatus(200)
+// })
+
 
 // io.on('connection',(socket)=>{
 //     console.log('a user connected')

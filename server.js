45922bd6591b85d85442d14dd7aacd7232498704
unknown <phucuong200297@gmail.com>
2022-03-01 13:04:38 +0700

    2 request

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const { header } = require('express/lib/request')
 const { response } = require('express')
 var app = express()
 var http = require('http').Server(app)
-var request = require('request');
+var request1 = require('request');
+var request2 = require('request');
 // var io = require('socket.io')(3000)
 // app.use(express.static(__dirname))
 app.use(bodyParser.json())
@@ -18,37 +19,35 @@ app.get('/', (req, res) => {
     var get_authorization_code = objectValue['code'];
     console.log("Authorization Code: " + get_authorization_code);
     // res.send(get_authorization_code);
-
-    
-    var options = {
-        'method': 'POST',
-        'url': 'https://oauth.zaloapp.com/v4/oa/access_token',
-        'headers': {
-            'secret_key': 'q52K4eXpUtLN353SVUcN',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        form: {
-            'app_id': '3264157168871710467',
-            'code_verifier': 'yWjvLbkuOMEZWUcMaPF43ChOldw8H87P_Zm813H5m1M',
-            'code': get_authorization_code,
-            'grant_type': 'authorization_code'
-        }
-    };
-    request(options, function (error, response) {
-        if (error) throw new Error(error);
-        console.log(response.body);
-        var infor = JSON.parse(response.body);
-        ac_token = infor.access_token
-        var rf_token = infor.refresh_token
-        var messages = {
-            'au_code': get_authorization_code,
-            'ac_token': ac_token,
-            'rf_token': rf_token
-        }
-        res.send(messages)
-    });
 })
 
+var options = {
+    'method': 'POST',
+    'url': 'https://oauth.zaloapp.com/v4/oa/access_token',
+    'headers': {
+        'secret_key': 'q52K4eXpUtLN353SVUcN',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+        'app_id': '3264157168871710467',
+        'code_verifier': 'yWjvLbkuOMEZWUcMaPF43ChOldw8H87P_Zm813H5m1M',
+        'code': get_authorization_code,
+        'grant_type': 'authorization_code'
+    }
+};
+request1(options, function (error, response) {
+    if (error) throw new Error(error);
+    console.log(response.body);
+    var infor = JSON.parse(response.body);
+    ac_token = infor.access_token
+    var rf_token = infor.refresh_token
+    var messages = {
+        'au_code': get_authorization_code,
+        'ac_token': ac_token,
+        'rf_token': rf_token
+    }
+    res.send(messages)
+});
 
 var option_follower = {
     'method': 'GET',
@@ -62,7 +61,7 @@ var option_follower = {
         "count": 10
     })
 };
-request(option_follower, function (error, response) {
+request2(option_follower, function (error, response) {
     if (error) throw new Error(error);
     console.log(response.body);
     res.send(JSON.stringify(response.body))

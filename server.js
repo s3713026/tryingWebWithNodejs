c74f3513da9f19304324f1f9b68174da363db00b
unknown <phucuong200297@gmail.com>
2022-03-02 14:31:48 +0700

    888

--- server.js
+++ server.js
@@ -187,13 +187,13 @@ app.post('/webhook', (req, res) => {
     if (body.object === 'page') {
   
       // Iterates over each entry - there may be multiple if batched
-      body.entry.forEach(function(entry) {
+    //   body.entry.forEach(function(entry) {
   
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-      });
+    //     // Gets the message. entry.messaging is an array, but 
+    //     // will only ever contain one message, so we get index 0
+    //     let webhook_event = entry.messaging[0];
+    //     console.log(webhook_event);
+    //   });
   
       // Returns a '200 OK' response to all requests
       res.status(200).send('EVENT_RECEIVED');

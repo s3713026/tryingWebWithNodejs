731bcb2c5e5be5b61bd0a18edb659f76b8153cf4
unknown <phucuong200297@gmail.com>
2022-03-03 11:08:12 +0700

    8686

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { json } = require('body-parser')
 const { send } = require('process')
 var http = require('http').Server(app)
 // var io = require('socket.io')(3000)
-app.use(express.static(__dirname))
+// app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
